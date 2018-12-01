2385d497f462e2736a9dce203a04a34e0ae7b92b
Jkb <jkb@jkbworld.com>
2018-12-01 15:08:54 -0800

    Initial Commit of hello_magento.py

---
+++ README.md
@@ -0,0 +1,18 @@
+Hello_Magento
+
+Intro:
+This is my "Hello World" for hacking around with the Magento2 Rest API's.  Apparently, these are Oauth1
+
+
+Reference:
+* Requests_oauthlib basically does oauth 1 and 2 on top of Requests.  Cool:  https://github.com/requests/requests-oauthlib 
+* Magento 2 API Status Codes (and a few samples) https://devdocs.magento.com/guides/v2.2/get-started/gs-web-api-response.html
+
+
+TODO:
+* Enable Magento2 API endpoints: https://inviqa.com/blog/magento-2-tutorial-overview-web-api
+* (Not used) Authenticate with Python Rauth: https://github.com/litl/rauth
+* Get list of all customers
+* Get list of all attributes
+* Get a specific attribute
+
---
+++ hello_magento.py
@@ -0,0 +1,53 @@
+from requests_oauthlib import OAuth1Session
+import pprint
+import json
+import sys
+import yaml
+
+#
+# Read the Magento API Oauth1 Info from the YAML file.
+#
+oc_key = oc_secret = oa_token = oa_secret = api_url = ''
+if len(sys.argv) != 2:
+    print("Usage:  hello_magento.py <config>.yaml")
+    exit(1)
+else:
+    try:
+        print("Using Configuration File: " + sys.argv[1])
+        with open(sys.argv[1], 'r') as f:
+            yml = yaml.load(f)
+            oc_key = yml['magento_api_settings']['consumer_key']
+            oc_secret = yml['magento_api_settings']['consumer_secret']
+            oa_token = yml['magento_api_settings']['access_token']
+            oa_secret = yml['magento_api_settings']['access_secret']
+            api_url = yml['magento_api_settings']['api_endpoint']
+
+    except IOError:
+        print("Error Opening Config File: " + sys.argv[1])
+        exit(1)
+
+#
+# Create the Oauth1 Session
+#
+m = OAuth1Session(oc_key,
+                        client_secret=oc_secret,
+                        resource_owner_key=oa_token,
+                        resource_owner_secret=oa_secret)
+
+#
+# Now run some tests and lookups...
+#
+
+# Call some individual customers
+# url = '{}/V1/customers/2'.format(API_URL)
+# url = '{}/V1/customers/1'.format(API_URL)
+
+# Get a specific product by SKU:
+sku = "F-AZAR-DR-SABL"
+url = '{}/V1/products/{}'.format(api_url, sku)
+
+#url = '{}/V1/categories/attributes/wood_finish'.format(API_URL)
+r = m.get(url)
+j = json.loads(r.text)
+pprint.pprint(r)
+pprint.pprint(j)
